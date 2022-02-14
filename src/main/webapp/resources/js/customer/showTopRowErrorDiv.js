8e3860f2e78c1e3317502dd4bfb361661ceb570d
autroshot <autroshot@gmail.com>
2022-02-14 16:02:24 +0900

    유효성 검사 리팩토링, 비밀번호 암호화 구현

--- src/main/webapp/resources/js/customer/showTopRowErrorDiv.js
+++ src/main/webapp/resources/js/customer/showTopRowErrorDiv.js
@@ -3,12 +3,14 @@
  * flag가 true이면 divElement의 바로 이전에 있는 엘리먼트의 클래스에서 row-other를 제거하고
  *  divElement의 hidden 속성을 제거해서 보이게 한다.
  */
-function showErrorDiv(errorDiv, flag) {
+function showTopRowErrorDiv(errorDiv, flag) {
 	if (flag) {
-		errorDiv.prev().removeClass("row-other");
+		errorDiv.prev().removeClass("row-top");
+		errorDiv.prev().addClass("row-top-without-border-bottom");
 		errorDiv.prop("hidden", false);
 	} else {
-		errorDiv.prev().addClass("row-other");
+		errorDiv.prev().removeClass("row-top-without-border-bottom");
+		errorDiv.prev().addClass("row-top");
 		errorDiv.prop("hidden", true);
 	}
 }
